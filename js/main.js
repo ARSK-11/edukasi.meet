5f4e46e4c2ec294f09f2104ae5713b150016af6c
ariskrisnanto <77524165+ARSK-11@users.noreply.github.com>
2021-11-22 06:42:17 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function btnUrl() {
-	var url = "https://meet.google.com/vsj-qqws-ust";
+	var url = "https://workspace.google.com/intl/en_id/products/meet/?utm_source=google&utm_medium=cpc&utm_campaign=1010746-Workspace-APAC-ID-en-BKWS-EXA-Golden&utm_content=text-ad-none-none-DEV_c-CRE_469259699742-ADGP_Hybrid%20%7C%20BKWS%20-%20EXA%20%7C%20Txt%20~%20Meet-KWID_43700057491415746-kwd-299356619278&userloc_1007719-network_g&utm_term=KW_google%20meet&gclid=Cj0KCQiA-eeMBhCpARIsAAZfxZDWbgrJjTDIjlPvebhyu7kwdy2vckfIuC1m5qyxCYzZNp6P1K2rvO0aAsHoEALw_wcB&gclsrc=aw.ds";
 	window.location.href = url;
 }
